0475834c9ea8e548655ae579cc11e6f9ec8269d2
baochunyu <baochunyu@nonobank.com>
2016-10-16 18:31:58 +0800

    init

---
+++ src/main/java/com/bao/MsNettyApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNettyApplication.class, args);
+	}
+}
